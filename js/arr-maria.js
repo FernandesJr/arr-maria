3388aabf190b227add47faea12df36519c250d5e
Fernandes Júnior <fernaandes.jr@gmail.com>
2023-04-25 16:35:07 -0300

    feat: animation and images at back of the to how

--- js/arr-maria.js
+++ js/arr-maria.js
@@ -1,12 +1,25 @@
-
+/*Show opacity of elements 'beneficios'*/
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('hidden-off');
     }
   });
-}, {threshold: 1});
+}, {threshold: 1}); //Only show the element when all visible
 
 Array.from(document.querySelectorAll('.hidden')).forEach(element => {
     observer.observe(element)
+});
+
+/*slide top of elements 'to whon'*/
+const observerHow = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-top');
+    }
+  });
+}, {threshold: 1}); //Only show the element when all visible
+
+Array.from(document.querySelectorAll('.banner-who')).forEach(element => {
+  observerHow.observe(element)
 });
