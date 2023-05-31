6ac3d0b73a52f96b19363c019aec9c2ba130a147
Fernandes Júnior <fernaandes.jr@gmail.com>
2023-05-31 14:53:48 -0300

    feat: new player video

--- js/arr-maria.js
+++ js/arr-maria.js
@@ -1,3 +1,5 @@
+let playFirstVideoOneTime = false;
+
 /*slide top of elements 'woman'*/
 const observerHoman = new IntersectionObserver(entries => {
   entries.forEach(entry => {
@@ -26,3 +28,26 @@ const observerBenefit = new IntersectionObserver(entries => {
 Array.from(document.querySelectorAll('.card-benefit-p')).forEach(element => {
   observerBenefit.observe(element);
 });
+
+/*Autoplay on the first video*/
+const observerVideo = new IntersectionObserver(entries => {
+  entries.forEach( entry => {
+    if (entry.isIntersecting && !playFirstVideoOneTime) {
+      playVideo(1);
+      playFirstVideoOneTime = true;
+    }
+  })
+  
+});
+
+const firstVideo = document.getElementById('video-1');
+observerVideo.observe(firstVideo);
+
+
+function playVideo(id) {
+  let icon = document.getElementById('icon-play-'+ id);
+  let video = document.getElementById('video-' + id);
+  icon.remove();
+  video.play();
+  video.setAttribute('controls', '');
+}
